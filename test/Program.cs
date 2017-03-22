8106cb9799d43788c5bdae261ea82762ac4bce11
Andreas Jung <info@zopyx.com>
2017-03-22 10:59:23 +0900

    pdatwd

--- test/Program.cs
+++ test/Program.cs
@@ -1,5 +1,9 @@
 using RestSharp;
 using System;
+using System.Collections.Generic;
+using JWT;
+using Jose;
+
  
 public class HelloWorld
 {
@@ -11,17 +15,38 @@ public class HelloWorld
 		private string _partner_url;
 		private bool _debug;
 
-        public SMASHDOCs(string client_id, string client_key, string partner_url, Boolean debug) {
+        public SMASHDOCs(string client_id, string client_key, string partner_url, bool debug) {
 			_client_id = client_id;
 			_client_key = client_key;
 			_partner_url = partner_url;
 			_debug = debug;
         }
 
-        public void bar() {
+		static long ToUnixTime(DateTime dateTime)
+		{
+			return (int)(dateTime.ToUniversalTime().Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+		}
 
-			Console.WriteLine(_partner_url);
-            Console.WriteLine("Bar");
+		private string get_token()
+		{
+			DateTime issued = DateTime.Now;
+			string iss = Guid.NewGuid().ToString();
+			string iat = ToUnixTime(issued).ToString();
+			string jti = Guid.NewGuid().ToString();
+										
+			var payload = new Dictionary<string, object>()
+			{
+				{"iss", iss},
+				{"iat", iat},
+				{"jti", jti}
+			};
+
+			return Jose.JWT.Encode(payload, _client_key, JwsAlgorithm.HS256);
+		}
+
+        public string new_document() {
+
+			Console.WriteLine(get_token());
 
             var client = new RestClient();
             client.BaseUrl = new Uri("http://zopyx.com");
@@ -33,6 +58,7 @@ public class HelloWorld
 			Console.WriteLine("{0}", response.ResponseStatus);
 			Console.WriteLine("{0}", (int)response.StatusCode); 
 			Console.WriteLine("{0}", response.Content);
+			return "foo";
       }
 
     }
@@ -42,15 +68,19 @@ public class HelloWorld
 		string client_id = Environment.GetEnvironmentVariable("SMASHDOCS_CLIENT_ID");
 		string client_key = Environment.GetEnvironmentVariable("SMASHDOCS_CLIENT_KEY");
 		string partner_url = Environment.GetEnvironmentVariable("SMASHDOCS_PARTNER_URL");
-		bool debug = Environment.GetEnvironmentVariable("SMASHDOCS_DEBUG").Length >0;
+		string debug = Environment.GetEnvironmentVariable("SMASHDOCS_DEBUG");
+		Console.WriteLine("Length {0}");
+		Console.WriteLine("Length {0}", debug.Length);
 
 		Console.WriteLine("Client ID: {0}", client_id);
 		Console.WriteLine("Client KEY: {0}", client_key);
 		Console.WriteLine("Partner URL: {0}", partner_url);
 		Console.WriteLine("DEBUG: {0}", debug);
 
-        SMASHDOCs f1 = new SMASHDOCs(client_id, client_key, partner_url, debug);
-		f1.bar();
+		bool _debug = true;
+
+        SMASHDOCs sd = new SMASHDOCs(client_id, client_key, partner_url, _debug);
+		var result = sd.new_document();
 
     }
 }
