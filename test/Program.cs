ea922248d392e128274f2f4e37e440d9b151e459
Andreas Jung <info@zopyx.com>
2017-04-02 11:20:15 +0200

    updated

--- test/Program.cs
+++ test/Program.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Jose;
 using System.Text;
+using System.IO;
 using RestSharp.Deserializers;
 using RestSharp.Serializers;
 using Newtonsoft.Json;
@@ -55,96 +56,118 @@ public class HelloWorld
 			int status_code = (int)response.StatusCode;
 			if (status_code != 200)
 			{
+				Console.WriteLine("Error:");
+				Console.WriteLine(response.Content);
 				throw new Exception($"Status code: {status_code}");
 			}
 		}
 
-
-		public JObject document_info(string document_id)
+		public IRestResponse make_request(string url, Method method, Dictionary<string, object> data = null)
 		{
 
 			var client = new RestClient();
 			client.BaseUrl = new Uri(_partner_url);
-			string url =  $"/partner/documents/{document_id}";
-	
-			var request = new RestRequest(url);
+			var request = new RestRequest(url, method);
+
 			request.AddHeader("Content-Type", "application/json");
 			request.AddHeader("x-client-id", _client_id);
 			request.AddHeader("Authorization", "Bearer " + get_token());
-
+			if (data != null)
+			{
+				request.AddJsonBody(data);
+			}
 			IRestResponse response = client.Execute(request);
 			check_response(response);
-			JObject result = JObject.Parse(response.Content);
-			return result;
+			return response;
+		}
+
+		public JObject document_info(string document_id)
+		{
+			string url =  $"/partner/documents/{document_id}";
+			IRestResponse response = make_request(url, Method.GET);
+			return JObject.Parse(response.Content);
 		}
 
 		public void  delete_document(string document_id)
 		{
-
-			var client = new RestClient(_partner_url);
 			string url = $"/partner/documents/{document_id}";
-			var request = new RestRequest(url, Method.DELETE);
-			request.AddHeader("Content-Type", "application/json");
-			request.AddHeader("x-client-id", _client_id);
-			request.AddHeader("Authorization", "Bearer " + get_token());
-
-			IRestResponse response = client.Execute(request);
-			check_response(response);
+			make_request(url, Method.DELETE);
 		}
 
 		public void archive_document(string document_id)
-		{
-
-			var client = new RestClient(_partner_url);
-			string url = $"/partner/documents/{document_id}/archive";
-
-			var request = new RestRequest(url, Method.POST);
-			request.AddHeader("Content-Type", "application/json");
-			request.AddHeader("x-client-id", _client_id);
-			request.AddHeader("Authorization", "Bearer " + get_token());
-
-			IRestResponse response = client.Execute(request);
-			check_response(response);
-
-		}
+			{
+				string url = $"/partner/documents/{document_id}/archive";
+				make_request(url, Method.POST);
+			}
+			
 
 		public void unarchive_document(string document_id)
 		{
-
-			var client = new RestClient(_partner_url);
 			string url = $"/partner/documents/{document_id}/unarchive";
-
-			var request = new RestRequest(url, Method.POST);
-			request.AddHeader("Content-Type", "application/json");
-			request.AddHeader("x-client-id", _client_id);
-			request.AddHeader("Authorization", "Bearer " + get_token());
-
-			IRestResponse response = client.Execute(request);
-			check_response(response);
-
+			make_request(url, Method.POST);
 		}
 
 		public JArray list_templates() {
-
-            var client = new RestClient();
-			client.BaseUrl = new Uri(_partner_url);
-            var request = new RestRequest();
-			request.Resource = "/partner/templates/word";
-			request.AddHeader("Content-Type", "application/json");
-			request.AddHeader("x-client-id", _client_id);
-			request.AddHeader("Authorization", "Bearer " + get_token());
-
-            IRestResponse response = client.Execute(request);
-			check_response(response);
-			Console.WriteLine(response.Content);
-			JArray result = JArray.Parse(response.Content);
-			return result;
-      }
+			IRestResponse response = make_request("/partner/templates/word", Method.GET);
+			return  JArray.Parse(response.Content);
+        }
 
 
 		public string export_document(string document_id, string user_id, string template_id="", string format = "docx", Dictionary<string, string> settings=null)
 		{
-			return "foo";
+			var data = new Dictionary<string, object>()
+			{
+				{"userId", user_id}
+			};
+
+			string url = "";
+			if (format == "html")
+			{
+				url = $"/partner/documents/{document_id}/export/html";
+			}
+			else if (format == "sdxml")
+			{
+				url = $"/partner/documents/{document_id}/export/sdxml";
+			}
+			else if (format == "docx")
+			{
+				url = $"/partner/documents/{document_id}/export/word";
+				if (settings == null)
+				{
+					data["settings"] = new Dictionary<string, string>();
+				}
+				else			{
+					data["settings"] = settings;
+				}
+				data["templateId"] = template_id;
+			}
+			else
+			{
+				throw new Exception($"Unknown format {format}");
+			}
+
+
+			var client = new RestClient();
+			client.BaseUrl = new Uri(_partner_url);
+			var request = new RestRequest(Method.POST);
+			Console.WriteLine(url); 
+			request.Resource = url;
+			request.AddHeader("Content-Type", "application/json");
+			request.AddHeader("x-client-id", _client_id);
+			request.AddHeader("Authorization", "Bearer " + get_token());
+			request.AddJsonBody(data);
+
+			IRestResponse response = client.Execute(request);
+			check_response(response);
+
+			string ext = (format == "docx") ? ".docx" : ".zip";
+			string fn = System.IO.Path.GetTempPath() + Guid.NewGuid().ToString() + ext;
+			FileStream fs = new FileStream(fn, FileMode.CreateNew);
+			BinaryWriter bw = new BinaryWriter(fs);
+			bw.Write(response.RawBytes);
+			bw.Close();
+			fs.Close();
+			return fn;
 		}
 
 		public JObject new_document(string title="", string description="", string role="editor", string status="draft")
@@ -167,21 +190,10 @@ public class HelloWorld
 				{"status", status},
 				{"sectionHistory", true}
 			};
-			var client = new RestClient();
-			client.BaseUrl = new Uri(_partner_url);
-			var request = new RestRequest(Method.POST);
-			request.Resource = "/partner/documents/create";
-			request.AddHeader("Content-Type", "application/json");
-			request.AddHeader("x-client-id", _client_id);
-			request.AddHeader("Authorization", "Bearer " + get_token());
-			request.AddJsonBody(data);
-						       
-			IRestResponse response = client.Execute(request);
-			check_response(response);
-			JObject result = JObject.Parse(response.Content);
-			return result;
-		}
 
+			IRestResponse response = make_request("/partner/documents/create", Method.POST, data);
+			return JObject.Parse(response.Content);
+		}
     }
 
     static public void Main ()
@@ -203,8 +215,8 @@ public class HelloWorld
 
 
 		var sd = new SMASHDOCs(client_id, client_key, partner_url, debug);
-		JArray result = sd.list_templates();
-		Console.WriteLine(result);
+		JArray templates = sd.list_templates();
+		Console.WriteLine(templates);
 	    JObject result2  = sd.new_document("my title", "my description");
 		Console.WriteLine(result2);
 		string document_id = (string) result2["documentId"];
@@ -214,11 +226,12 @@ public class HelloWorld
 		sd.archive_document(document_id);
 		sd.unarchive_document(document_id);
 
+
+		Console.WriteLine(sd.export_document(document_id, user_id: "ajung", template_id: "", format: "html"));
+		Console.WriteLine(sd.export_document(document_id, user_id: "ajung", template_id: "", format: "sdxml"));
+		string template_id = (string) templates[0]["id"];
+		Console.WriteLine(sd.export_document(document_id, user_id: "ajung", template_id: template_id, format: "docx"));
+
 		sd.delete_document(document_id);
-		metadata = sd.document_info(document_id);
-
-
-
-
      }
 }
